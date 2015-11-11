98e462baadd1f747ca5ab561ac477eeed8cb86a9
Corentin Mathé--Deletang <corentin.mathe@cri-paris.org>
2015-11-11 17:24:25 +0100

    commit25

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,4 +1,4 @@
 import datetime
-n = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
-m = datetime.datetime.strftime(datetime.datetime.now(), '%H-%M-%S')
+n = datetime.datetime.strftime(datetime.datetime.now(), '%Y:%m:%d')
+m = datetime.datetime.strftime(datetime.datetime.now(), '%X')
 print("Today is " +  n + " and it is " + m)
