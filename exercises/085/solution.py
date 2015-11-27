bd65c9ed1e3c6cbd2a129556eaed328b77188642
Corentin Mathé--Deletang <corentin.mathe@cri-paris.org>
2015-11-27 16:50:52 +0100

    commit085 third try

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -3,11 +3,12 @@ def sort_a_list(L):
     return(M)
 
 import operator
+
+
 def sort_by_mark(myclass):
     myclass = sorted(myclass, key=operator.itemgetter(0), reverse=True)
     return(myclass)
 
-import operator
 def sort_by_name(myclass):
     myclass = sorted(myclass, key=operator.itemgetter(1), reverse=True)
-    return(myclass) 
+    return(myclass)
